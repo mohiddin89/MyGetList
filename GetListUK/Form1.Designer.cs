738d799fabb98e12ea56543fe65032aa2df612fe
khurshid27 <47109163+khurshid27@users.noreply.github.com>
2019-02-14 18:39:42 +0530

    Add project files.

---
+++ GetListUK/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace GetListUK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtReport = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btninputCSV = new System.Windows.Forms.Button();
+            this.btninputReport = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(127, 28);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(200, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtReport
+            // 
+            this.txtReport.Location = new System.Drawing.Point(125, 92);
+            this.txtReport.Name = "txtReport";
+            this.txtReport.Size = new System.Drawing.Size(200, 20);
+            this.txtReport.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input CSV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Report";
+            // 
+            // btninputCSV
+            // 
+            this.btninputCSV.Location = new System.Drawing.Point(360, 26);
+            this.btninputCSV.Name = "btninputCSV";
+            this.btninputCSV.Size = new System.Drawing.Size(50, 23);
+            this.btninputCSV.TabIndex = 4;
+            this.btninputCSV.Text = "-----";
+            this.btninputCSV.UseVisualStyleBackColor = true;
+            this.btninputCSV.Click += new System.EventHandler(this.btninputCSV_Click);
+            // 
+            // btninputReport
+            // 
+            this.btninputReport.Location = new System.Drawing.Point(360, 87);
+            this.btninputReport.Name = "btninputReport";
+            this.btninputReport.Size = new System.Drawing.Size(50, 23);
+            this.btninputReport.TabIndex = 5;
+            this.btninputReport.Text = "-----";
+            this.btninputReport.UseVisualStyleBackColor = true;
+            this.btninputReport.Click += new System.EventHandler(this.btninputReport_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(15, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Gethref";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 224);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btninputReport);
+            this.Controls.Add(this.btninputCSV);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtReport);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtReport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btninputCSV;
+        private System.Windows.Forms.Button btninputReport;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ GetListUK/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SharePoint;
+using System.IO;
+
+namespace GetListUK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<string> listColl = new List<string>();
+            StreamReader sr = new StreamReader(System.IO.File.OpenRead(txtInput.Text));
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    listColl.Add(sr.ReadLine().Trim());
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+
+            StreamWriter ExcelwriterScoringMatrixNew = null;
+            ExcelwriterScoringMatrixNew = System.IO.File.CreateText(txtReport.Text + "\\" + "ScoringMatrix" + "_" + DateTime.Now.ToString("dd-mm-yyyy-hh-mm-ss") + ".csv");
+            ExcelwriterScoringMatrixNew.WriteLine("SiteURL" +","+ "PageName" +","+ "PageUrl" +","+ "PageId" +","+ "ListType" +","+ "Type" +","+ "webpart" +","+ "url");
+            ExcelwriterScoringMatrixNew.Flush();
+        }
+
+        private void btninputCSV_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog inputfile = new OpenFileDialog();
+
+            if(inputfile.ShowDialog()==DialogResult.OK)
+            {
+                txtInput.Text = inputfile.FileName;
+            }
+            #region Commented
+            //inputfile.Multiselect = true;
+            //inputfile.ShowDialog();
+            //inputfile.Filter = "allfiles|*.xls";
+            //txtInput.Text = inputfile.FileName;
+            //int count = 0;
+            //string[] Fname;
+            //foreach(string s in inputfile.FileNames)
+            //{
+            //    Fname = s.Split('\\');
+            //    File.Copy(s,  Fname[Fname.Length - 1]);
+            //    count++;
+            //}
+            //MessageBox.Show(Convert.ToString(count) + " File(s) copied"); 
+            #endregion
+        }
+
+        private void btninputReport_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog reportFolder = new FolderBrowserDialog();
+
+            if(reportFolder.ShowDialog()==DialogResult.OK)
+            {
+                txtReport.Text = reportFolder.SelectedPath;
+            }
+            #region Commented Code
+            //reportFolder.ShowDialog();
+            //txtReport.Text = reportFolder.SelectedPath;            
+            //string path = txtReport.Text;
+            //StreamWriter sw = new StreamWriter(path);
+            //System.IO.File.WriteAllLines(path,"Reports.csv",) 
+            #endregion
+
+        }
+    }
+}
